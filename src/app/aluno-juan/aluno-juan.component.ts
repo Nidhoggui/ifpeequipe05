588a0c3eaaf1b94f27badab6c0c3373fcb9c3e57
Juan Gustavo <juangt360graus@gmail.com>
2019-09-11 08:28:30 -0300

    adicionado componente AlunoJuan

---
+++ src/app/aluno-juan/aluno-juan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aluno-juan',
+  templateUrl: './aluno-juan.component.html',
+  styleUrls: ['./aluno-juan.component.css']
+})
+export class AlunoJuanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlunoJuanComponent } from './aluno-juan/aluno-juan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunoJuanComponent
   ],
   imports: [
     BrowserModule
